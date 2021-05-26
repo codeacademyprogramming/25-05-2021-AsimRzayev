a7a5e5f24dd01564c174ed4579bdc5e4ef3372a7
AsimRzayev <asimrr@code.edu.az>
2021-05-26 06:47:47 +0400

    FixFormmating

--- src/components/Product/index.js
+++ src/components/Product/index.js
@@ -1,13 +1,18 @@
-import React from 'react'
-import { useGlobalContext} from '../../contexts/globContext'
-import ProductItem from './ProductItem';
+import React,{useEffect} from "react";
+import { useDispatch } from "react-redux";
+import { useGlobalContext } from "../../contexts/globContext";
+import { useSelector } from 'react-redux'
+import ProductItem from "./ProductItem";
 export default function Product() {
-    const {state:{products}}=useGlobalContext();
- 
+const products = useSelector(state =>state.products)
+
+   
+           
     return (
-      <>
-      {products.map(product=>(<ProductItem key={product.id} datas={product} />))}
-    
-      </>
-    )
+        <>
+            {products.map((product) => (
+                <ProductItem key={product.id} datas={product} />
+            ))}
+        </>
+    );
 }
