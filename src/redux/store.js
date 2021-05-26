5ef10c49bdc03b12b3a3693213573d42c18e7250
AsimRzayev <asimrr@code.edu.az>
2021-05-26 06:13:19 +0400

    addLoggerReduxthunk

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,20 @@
-import {createStore} from "redux"
+import {createStore,applyMiddleware} from "redux"
 import reducer from './reducer';
-const  store=createStore(reducer);
+import reduxThunkMiddleware from "redux-thunk";
+
+
+const logger=storeAPI=>next=>action=> {
+ console.log('Store before action dispatch',storeAPI.getState());
+ console.log('Action Dispatch',action);
+const result=next(action)
+console.log('Store before action dispatch',storeAPI.getState());
+return result
+
+}
+
+const middlewares=applyMiddleware(
+    logger,
+    reduxThunkMiddleware
+)
+const  store=createStore(reducer,middlewares);
 export default store
